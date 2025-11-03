20f5a60d4882db94324caea0f99454debc277b0a
aashiq16 <87873115+aashiq16@users.noreply.github.com>
2025-11-03 21:16:43 +0530

    Update RAGGY.py

--- RAGGY.py
+++ RAGGY.py
@@ -11,8 +11,8 @@ from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
-from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
